9a646fd6436fc5e22e2b2a45ef5a69b1b915fef8
Pushpendra <kumarpushpendra0008989@gmail.com>
2026-03-01 15:23:13 +0530

    change style

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\StandardsController;
@@ -17,136 +15,97 @@ use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\GuardianController;
 use App\Http\Controllers\ChatController;
 
-// Route::apiResource('students', StudentController::class);
-Route::prefix('students')
-    ->controller(StudentController::class)
-    ->group(function () {
+// Grouped structure for middleware-based organization (no behavior change).
+Route::middleware([])->group(function () {
+    Route::prefix('students')->controller(StudentController::class)->group(function () {
         Route::post('/init', 'initStudents');
-    }
-);
+    });
 
-Route::prefix('services')
-    ->controller(ServicesController::class)
-    ->group(function () {
+    Route::prefix('services')->controller(ServicesController::class)->group(function () {
         Route::post('/init', 'initServices');
         Route::post('/store', 'storeService');
-    }
-);
+    });
 
-Route::prefix('standards')
-    ->controller(StandardsController::class)
-    ->group(function () {
+    Route::prefix('standards')->controller(StandardsController::class)->group(function () {
         Route::post('/init', 'initStandards');
         Route::post('/store', 'storeStandard');
         Route::post('/delete', 'deleteStandard');
-    }
-);
+    });
 
-Route::prefix('sections')
-    ->controller(SectionsController::class)
-    ->group(function () {
+    Route::prefix('sections')->controller(SectionsController::class)->group(function () {
         Route::post('/init', 'initSections');
         Route::post('/store', 'storeSection');
         Route::post('/delete', 'deleteSection');
-    }
-);
+    });
 
-Route::prefix('subjects')
-    ->controller(SubjectsController::class)
-    ->group(function () {
+    Route::prefix('subjects')->controller(SubjectsController::class)->group(function () {
         Route::post('/init', 'initSubjects');
         Route::post('/store', 'storeSubject');
         Route::post('/delete', 'deleteSubject');
-    }
-);
+    });
 
-Route::prefix('fee-types')
-    ->controller(FeeTypesController::class)
-    ->group(function () {
+    Route::prefix('fee-types')->controller(FeeTypesController::class)->group(function () {
         Route::post('/init', 'initFeeTypes');
         Route::post('/store', 'storeFeeType');
         Route::post('/delete', 'deleteFeeType');
-    }
-);
+    });
 
-Route::prefix('dashboard')
-    ->controller(SuperAdminController::class)
-    ->group(function () {
+    Route::prefix('dashboard')->controller(SuperAdminController::class)->group(function () {
         Route::post('/init', 'initDashboard');
-    }
-);
+    });
 
-Route::prefix('admin/dashboard')
-    ->controller(AdminController::class)
-    ->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::prefix('dashboard')->controller(AdminController::class)->group(function () {
+            Route::post('/init', 'initDashboard');
+        });
+
+        Route::controller(AdminController::class)->group(function () {
+            Route::post('/students/init', 'initStudents');
+            Route::post('/students/get', 'getStudent');
+            Route::post('/students/store', 'storeStudent');
+            Route::post('/students/status', 'updateStudentStatus');
+            Route::post('/teachers/init', 'initTeachers');
+            Route::post('/teachers/get', 'getTeacher');
+            Route::post('/teachers/store', 'storeTeacher');
+        });
+
+        Route::controller(IncomeController::class)->group(function () {
+            Route::post('/incomes/init', 'initIncomes');
+            Route::post('/incomes/store', 'storeIncome');
+            Route::post('/income-entries/init', 'initIncomeEntries');
+            Route::post('/income-entries/store', 'storeIncomeEntry');
+            Route::post('/expenses/init', 'initExpenses');
+            Route::post('/expenses/store', 'storeExpense');
+            Route::post('/expense-entries/init', 'initExpenseEntries');
+            Route::post('/expense-entries/store', 'storeExpenseEntry');
+        });
+
+        Route::controller(AttendanceController::class)->group(function () {
+            Route::post('/attendance/init', 'init');
+            Route::post('/attendance/store', 'store');
+            Route::post('/attendance/list', 'list');
+        });
+    });
+
+    Route::prefix('teachers')->group(function () {
+        Route::prefix('dashboard')->controller(TeacherController::class)->group(function () {
+            Route::post('/init', 'initDashboard');
+        });
+
+        Route::prefix('exam-marks')->controller(TeacherController::class)->group(function () {
+            Route::post('/init', 'initExamMarks');
+            Route::post('/store', 'storeExamMark');
+            Route::post('/list', 'listExamMarks');
+        });
+    });
+
+    Route::prefix('gurdian/dashboard')->controller(GuardianController::class)->group(function () {
         Route::post('/init', 'initDashboard');
-    }
-);
+    });
 
-Route::prefix('admin')
-    ->controller(AdminController::class)
-    ->group(function () {
-        Route::post('/students/init', 'initStudents');
-        Route::post('/students/get', 'getStudent');
-        Route::post('/students/store', 'storeStudent');
-        Route::post('/students/status', 'updateStudentStatus');
-        Route::post('/teachers/init', 'initTeachers');
-        Route::post('/teachers/get', 'getTeacher');
-        Route::post('/teachers/store', 'storeTeacher');
-    }
-);
-
-Route::prefix('admin')
-    ->controller(IncomeController::class)
-    ->group(function () {
-        Route::post('/incomes/init', 'initIncomes');
-        Route::post('/incomes/store', 'storeIncome');
-        Route::post('/income-entries/init', 'initIncomeEntries');
-        Route::post('/income-entries/store', 'storeIncomeEntry');
-        Route::post('/expenses/init', 'initExpenses');
-        Route::post('/expenses/store', 'storeExpense');
-        Route::post('/expense-entries/init', 'initExpenseEntries');
-        Route::post('/expense-entries/store', 'storeExpenseEntry');
-    }
-);
-
-Route::prefix('admin')
-    ->controller(AttendanceController::class)
-    ->group(function () {
-        Route::post('/attendance/init', 'init');
-        Route::post('/attendance/store', 'store');
-        Route::post('/attendance/list', 'list');
-    }
-);
-
-Route::prefix('teachers/dashboard')
-    ->controller(TeacherController::class)
-    ->group(function () {
-        Route::post('/init', 'initDashboard');
-    }
-);
-
-Route::prefix('teachers/exam-marks')
-    ->controller(TeacherController::class)
-    ->group(function () {
-        Route::post('/init', 'initExamMarks');
-        Route::post('/store', 'storeExamMark');
-        Route::post('/list', 'listExamMarks');
-    }
-);
-
-Route::prefix('gurdian/dashboard')
-    ->controller(GuardianController::class)
-    ->group(function () {
-        Route::post('/init', 'initDashboard');
-    }
-);
-
-Route::prefix('chat')
-    ->controller(ChatController::class)
-    ->group(function () {
+    Route::prefix('chat')->controller(ChatController::class)->group(function () {
         Route::post('/init', 'init');
         Route::post('/thread', 'thread');
         Route::post('/send', 'send');
-    }
-);
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -17,123 +17,89 @@ use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\GuardianController;
 use App\Http\Controllers\ChatController;
 
-Route::get('/', [UserController::class,'login'])->name("login");
+Route::get('/', [UserController::class, 'login'])->name('login');
 Route::get('/login/captcha', [UserController::class, 'captcha'])->name('login.captcha');
-Route::post('/login', [UserController::class,'postLogin']);
+Route::post('/login', [UserController::class, 'postLogin']);
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
-Route::prefix('worklog')->middleware(['auth'])
-    ->controller(WorklogController::class)
-    ->group(function () {
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('worklog')->controller(WorklogController::class)->group(function () {
         Route::get('/', 'index')->name('worklog.index');
         Route::post('/store', 'store')->name('worklog.store');
-    }
-);
+    });
 
-Route::prefix('students')->middleware(['auth'])
-    ->controller(StudentController::class)
-    ->group(function () {
+    Route::prefix('students')->controller(StudentController::class)->group(function () {
         Route::get('/', 'index');
-    }
-);
+    });
 
-Route::prefix('teachers')->middleware(['auth', 'teacher'])
-    ->controller(TeacherController::class)
-    ->group(function () {
+    Route::prefix('chat')->controller(ChatController::class)->group(function () {
+        Route::get('/', 'index')->name('chat.index');
+    });
+
+    Route::middleware(['teacher'])->prefix('teachers')->controller(TeacherController::class)->group(function () {
         Route::get('/dashboard', 'dashboard');
         Route::get('/exam-marks', 'examMarksPage')->name('teachers.exam-marks.index');
-    }
-);
+    });
 
-Route::prefix('gurdian')->middleware(['auth', 'guardian'])
-    ->controller(GuardianController::class)
-    ->group(function () {
+    Route::middleware(['guardian'])->prefix('gurdian')->controller(GuardianController::class)->group(function () {
         Route::get('/dashboard', 'dashboard');
-    }
-);
+    });
 
-Route::prefix('chat')->middleware(['auth'])
-    ->controller(ChatController::class)
-    ->group(function () {
-        Route::get('/', 'index')->name('chat.index');
-    }
-);
+    Route::middleware(['admin'])->prefix('admin')->group(function () {
+        Route::controller(AdminController::class)->group(function () {
+            Route::get('/dashboard', 'dashboard');
+            Route::get('/students', 'studentsIndex')->name('admin.students.index');
+            Route::get('/students/add/{student?}', 'addStudentPage')->name('admin.students.add');
+            Route::get('/students/profile/{student}', 'studentProfilePage')->name('admin.students.profile');
+            Route::get('/teachers', 'teachersIndex')->name('admin.teachers.index');
+            Route::get('/teachers/add/{teacher?}', 'addTeacherPage')->name('admin.teachers.add');
+        });
 
-Route::prefix('super-admin')->middleware(['auth', 'super-admin'])
-    ->controller(SuperAdminController::class)
-    ->group(function () {
-        Route::get('/dashboard', 'dashboard');
-        Route::get('/users/profile/{id}', 'userProfile')->name('super-admin.users.profile');
-        Route::get('/schools/create', 'createSchoolPage')->name('super-admin.schools.create-page');
-        Route::get('/schools/{id}/edit', 'editSchoolPage')->name('super-admin.schools.edit-page');
-        Route::get('/schools/{id}/services', 'schoolServicesPage')->name('super-admin.schools.services');
-        Route::post('/schools/{id}/services', 'saveSchoolServices')->name('super-admin.schools.services.save');
-        Route::get('/users', 'usersList')->name('super-admin.users');
-        Route::get('/users/{type}', 'usersByType')->name('super-admin.users.type');
-        Route::post('/users/{id}/status', 'updateUserStatus')->name('super-admin.users.status');
-        Route::post('/schools', 'createSchool')->name('super-admin.schools.create');
-        Route::post('/schools/{id}', 'updateSchool')->name('super-admin.schools.update');
-    }
-);
+        Route::controller(IncomeController::class)->group(function () {
+            Route::get('/incomes', 'incomesPage')->name('admin.incomes.index');
+            Route::get('/income-entries', 'incomeEntriesPage')->name('admin.income-entries.index');
+            Route::get('/expenses', 'expensesPage')->name('admin.expenses.index');
+            Route::get('/expense-entries', 'expenseEntriesPage')->name('admin.expense-entries.index');
+        });
 
-Route::prefix('admin')->middleware(['auth', 'admin'])
-    ->controller(AdminController::class)
-    ->group(function () {
-        Route::get('/dashboard', 'dashboard');
-        Route::get('/students', 'studentsIndex')->name('admin.students.index');
-        Route::get('/students/add/{student?}', 'addStudentPage')->name('admin.students.add');
-        Route::get('/students/profile/{student}', 'studentProfilePage')->name('admin.students.profile');
-        Route::get('/teachers', 'teachersIndex')->name('admin.teachers.index');
-        Route::get('/teachers/add/{teacher?}', 'addTeacherPage')->name('admin.teachers.add');
-    }
-);
+        Route::controller(AttendanceController::class)->group(function () {
+            Route::get('/attendance', 'index')->name('admin.attendance.index');
+        });
+    });
 
-Route::prefix('admin')->middleware(['auth', 'admin'])
-    ->controller(IncomeController::class)
-    ->group(function () {
-        Route::get('/incomes', 'incomesPage')->name('admin.incomes.index');
-        Route::get('/income-entries', 'incomeEntriesPage')->name('admin.income-entries.index');
-        Route::get('/expenses', 'expensesPage')->name('admin.expenses.index');
-        Route::get('/expense-entries', 'expenseEntriesPage')->name('admin.expense-entries.index');
-    }
-);
+    Route::middleware(['super-admin'])->prefix('super-admin')->group(function () {
+        Route::controller(SuperAdminController::class)->group(function () {
+            Route::get('/dashboard', 'dashboard');
+            Route::get('/users/profile/{id}', 'userProfile')->name('super-admin.users.profile');
+            Route::get('/schools/create', 'createSchoolPage')->name('super-admin.schools.create-page');
+            Route::get('/schools/{id}/edit', 'editSchoolPage')->name('super-admin.schools.edit-page');
+            Route::get('/schools/{id}/services', 'schoolServicesPage')->name('super-admin.schools.services');
+            Route::post('/schools/{id}/services', 'saveSchoolServices')->name('super-admin.schools.services.save');
+            Route::get('/users', 'usersList')->name('super-admin.users');
+            Route::get('/users/{type}', 'usersByType')->name('super-admin.users.type');
+            Route::post('/users/{id}/status', 'updateUserStatus')->name('super-admin.users.status');
+            Route::post('/schools', 'createSchool')->name('super-admin.schools.create');
+            Route::post('/schools/{id}', 'updateSchool')->name('super-admin.schools.update');
+        });
 
-Route::prefix('admin')->middleware(['auth', 'admin'])
-    ->controller(AttendanceController::class)
-    ->group(function () {
-        Route::get('/attendance', 'index')->name('admin.attendance.index');
-    }
-);
-
-Route::prefix('super-admin')->middleware(['auth', 'super-admin'])
-    ->controller(ServicesController::class)
-    ->group(function () {
+        Route::controller(ServicesController::class)->group(function () {
             Route::get('/services', 'index')->name('super-admin.services.index');
-    }
-);
+        });
 
-Route::prefix('super-admin')->middleware(['auth', 'super-admin'])
-    ->controller(StandardsController::class)
-    ->group(function () {
-        Route::get('/standards', 'index')->name('super-admin.standards.index');
-    }
-);
+        Route::controller(StandardsController::class)->group(function () {
+            Route::get('/standards', 'index')->name('super-admin.standards.index');
+        });
 
-Route::prefix('super-admin')->middleware(['auth', 'super-admin'])
-    ->controller(SectionsController::class)
-    ->group(function () {
-        Route::get('/sections', 'index')->name('super-admin.sections.index');
-    }
-);
+        Route::controller(SectionsController::class)->group(function () {
+            Route::get('/sections', 'index')->name('super-admin.sections.index');
+        });
 
-Route::prefix('super-admin')->middleware(['auth', 'super-admin'])
-    ->controller(SubjectsController::class)
-    ->group(function () {  
-        Route::get('/subjects', 'index')->name('super-admin.subjects.index');
-    }
-);
+        Route::controller(SubjectsController::class)->group(function () {
+            Route::get('/subjects', 'index')->name('super-admin.subjects.index');
+        });
 
-Route::prefix('super-admin')->middleware(['auth', 'super-admin'])->controller(FeeTypesController::class)->group(function () {
-        Route::get('/fee-types', 'index')->name('super-admin.fee-types.index');
-    }
-);
+        Route::controller(FeeTypesController::class)->group(function () {
+            Route::get('/fee-types', 'index')->name('super-admin.fee-types.index');
+        });
+    });
+});
