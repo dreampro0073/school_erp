95db7d1077310366f5aa23bf1e034eb34c1657f3
divandra00 <divandra00@gmail.com>
2026-03-19 19:38:26 +0530

    migration

--- database_migration.php
+++ database_migration.php
@@ -218,9 +218,9 @@ ALTER TABLE `students` CHANGE `admission_no` `admission_no` VARCHAR(50) NULL DEF
 ALTER TABLE `students` ADD `parent_user_id` INT NOT NULL DEFAULT '0' AFTER `user_id`;
 
 
-CREATE TABLE `shri_erp`.`master_data` ( `master_id` INT NOT NULL , `name` VARCHAR(255) NOT NULL , `type` INT NOT NULL DEFAULT '0' COMMENT '1-->Category ,2--> Blood Group , 3-->gender-->' , `status` TINYINT NOT NULL DEFAULT '0' COMMENT '0-->active, 1-->inactive' ) ENGINE = InnoDB;
+CREATE TABLE `master_data` ( `master_id` INT NOT NULL , `name` VARCHAR(255) NOT NULL , `type` INT NOT NULL DEFAULT '0' COMMENT '1-->Category ,2--> Blood Group , 3-->gender-->' , `status` TINYINT NOT NULL DEFAULT '0' COMMENT '0-->active, 1-->inactive' ) ENGINE = InnoDB;
 
-<<<<<<< HEAD
+
 ALTER TABLE `master_data` ADD PRIMARY KEY( `master_id`);
 ALTER TABLE `master_data` CHANGE `master_id` `master_id` INT(11) NOT NULL AUTO_INCREMENT;
 
@@ -239,7 +239,6 @@ ALTER TABLE `students` ADD `standard_id` INT NOT NULL DEFAULT '0' AFTER `erp_id`
 ALTER TABLE `students` ADD `height` VARCHAR(20) NULL DEFAULT NULL AFTER `blood_group_id`, ADD `weight` VARCHAR(20) NULL DEFAULT NULL AFTER `height`;
 
 ALTER TABLE `students` ADD `previous_school` VARCHAR(255) NULL DEFAULT NULL AFTER `approved`, ADD `previous_school_address` VARCHAR(255) NULL DEFAULT NULL AFTER `previous_school`;
-=======
 
 
->>>>>>> 6c97d384ac92adf1ce60c2d918be11d75fcb9b73
+
