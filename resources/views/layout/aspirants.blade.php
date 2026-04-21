379a7921e4424b62882c553c783c639489b1cc6e
divandra00 <divandra00@gmail.com>
2026-04-21 20:43:24 +0530

    testing

--- resources/views/layout/aspirants.blade.php
+++ resources/views/layout/aspirants.blade.php
@@ -1,27 +1,27 @@
 <li>
    <a href="{{ url('/aspirant/dashboard') }}">
-      <i class="ri-home-4-line animate-spin"></i>
+      <i class="ri-home-4-line "></i>
       <span>Dashboard</span>
    </a>
 </li>
 
 <li>
    <a href="{{ url('/aspirant/subjects/index') }}">
-      <i class="ri-book-3-line animate-spin"></i>
+      <i class="ri-book-3-line "></i>
       <span>Subjects</span>
    </a>
 </li>
 
 <li>
    <a href="{{ url('/aspirant/practice/index') }}">
-      <i class="ri-pencil-ruler-line animate-spin"></i>
+      <i class="ri-pencil-ruler-line"></i>
       <span>Practice</span>
    </a>
 </li>
 
 <li>
    <a href="{{ url('/aspirant/exams/index') }}">
-      <i class="ri-file-paper-2-line animate-spin"></i>
+      <i class="ri-file-paper-2-line"></i>
       <span>Exam</span>
    </a>
 </li>
